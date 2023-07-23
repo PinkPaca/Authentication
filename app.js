b9a5f513d692fdf1655f488407c1fe1ba12eca77
PinkPaca <fmadkdl113@gmail.com>
2023-07-23 15:24:38 +0100

    Level 1 - Username and Password Only

---
+++ app.js
@@ -0,0 +1,66 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.unsubscribe(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", async(req, res) => {
+  res.render("home");
+});
+
+app.get("/login", async(req, res) => {
+  res.render("login");
+});
+
+app.get("/register", async(req, res) => {
+  res.render("register");
+});
+
+app.post("/register", async(req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save();
+  res.render("secrets");
+});
+
+app.post("/login", async(req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const foundUser = await User.findOne({email: username});
+
+  if(foundUser) {
+    if(foundUser.password === password) {
+      res.render("secrets");
+    }else {
+      res.send("Your password is wrong");
+    }
+  } else {
+    res.send("Your e-mail doesn't exist")
+  }
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
